038cc6ddfd9f808d8b4f2769602ac61694f714e0
Seonghuichoe <heeday0410@gmail.com>
2018-11-09 22:47:17 +0900

    Remove useless code

--- src/components/todo-page/todo-page.component.ts
+++ src/components/todo-page/todo-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
